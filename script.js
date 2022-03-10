61154d070e4a459dc7554ee40bb46d30ef988df1
triangletrianglesquareplusx <69000271+triangletrianglesquareplusx@users.noreply.github.com>
2022-03-10 11:49:29 +0200

    implementing closing and opening modal

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let modalElement = document.querySelector('.modal');
+let overlay = document.querySelector('.overlay');
+let btnToCloseModal = document.querySelector('.close-modal');
+let btnToShowModal = document.querySelectorAll('.show-modal');
+const closeOutModalAndOverlay = ()=>{
+    modalElement.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+const openModalAndOverlay = ()=>{
+    modalElement.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+for(let i =0;i<btnToShowModal.length;i++){
+    btnToShowModal[i].addEventListener('click', openModalAndOverlay)
+}
+
+btnToCloseModal.addEventListener('click', closeOutModalAndOverlay);
+
+overlay.addEventListener('click', closeOutModalAndOverlay)
